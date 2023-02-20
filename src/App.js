a53b33b76b7b32e73598fbff050cccd7b192deaf
Your Name <you@example.com>
2023-02-20 18:51:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import CreateNotes from './components/CreateNotes';
+import NotesList from './components/NotesList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+            <h1>Notes Manager</h1>
+      </nav>
+      <CreateNotes />
+      <NotesList />
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import noteSlice from "./noteSlice";
+
+const store = configureStore({
+  reducer: {
+    noteSlice,
+  },
+});
+
+export default store;
