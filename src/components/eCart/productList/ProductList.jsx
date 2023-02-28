73855445ba8d4d31bbffb318aab4678659181eeb
Shuaib Siddiqui <122801841+sbsidd17@users.noreply.github.com>
2023-02-28 09:41:45 +0000

    used redux thunk

--- src/components/eCart/productList/ProductList.jsx
+++ src/components/eCart/productList/ProductList.jsx
@@ -2,32 +2,27 @@ import React from "react";
 import "./style.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { createProductList } from "../../../redux/productSlice";
+import { fetchData } from "../../../redux/productSlice";
 import SingleProduct from "../singleProduct/SingleProduct";
+import Loader from "../loading/Loader";
 
 function ProductList() {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.productSlice.productList);
   useEffect(() => {
-    getProducts();
+    dispatch(fetchData());
     // eslint-disable-next-line
   }, []);
-  
-  async function getProducts() {
-    const resp = await fetch("https://fakestoreapi.com/products");
-    const data = await resp.json();
-    dispatch(createProductList(data));
-  }
+
+  // const status = useSelector((state) => state.productSlice.status);
+  // if(status === 'loading'){
+  //   return <Loader />
+  // }
 
   return (
     <div className="productList">
       {products.map((item) => {
-        return (
-          <SingleProduct
-            key = {item.id}
-            product = {item}
-          />
-        );
+        return <SingleProduct key={item.id} product={item} />;
       })}
     </div>
   );
--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -1,17 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchData = createAsyncThunk("productList/fetch", async () => {
+  const resp = await fetch("https://fakestoreapi.com/products");
+  const data = await resp.json();
+  return data;
+});
 
 const productSlice = createSlice({
   name: "productSlice",
   initialState: {
-    count : 0,
     productList: [],
+    status: "idle",
+    error: null,
   },
   reducers: {
     createProductList: (state, action) => {
       state.productList = action.payload;
     },
   },
+  extraReducers: function (builder) {
+    builder
+      .addCase(fetchData.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchData.fulfilled, (state, action) => {
+        state.status = "fulfilled";
+        state.productList = action.payload;
+      })
+      .addCase(fetchData.error, (state, action) => {
+        state.status = "error";
+      });
+  },
 });
 
 export default productSlice.reducer;
-export const { createProductList, increaseProductCount, decreaseProductCount } = productSlice.actions;
+export const { createProductList, increaseProductCount, decreaseProductCount } =
+  productSlice.actions;
